003ad684edbffc100d0c7994d8be8859936fcaa6
unknown <nmukhtar2001@gmail.com>
2023-03-01 21:25:56 -0800

    did index.js not done

---
+++ inden.js
@@ -0,0 +1,42 @@
+const lib = require ("./lib");
+
+
+
+const questions =[
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+      },
+      {
+        type: 'input',
+        message: 'What is your employee ID?',
+        name: 'employee ID',
+      },
+      {
+        type: 'input',
+        message: 'what is your job title :',
+        name: 'title',
+      },
+      {
+        type: 'input',
+        message: 'What is your email ?',
+        name:'email'
+      },
+      {
+        type: 'input',
+        message: 'What is your GitHub username ?',
+        name:'GitHub username'
+      },
+      {
+        type: 'input',
+        message: 'What is your office number ?',
+        name:'office number'
+      },
+      {
+        type: 'input',
+        message: 'What is your school name?',
+        name:'school name '
+      }
+
+]
