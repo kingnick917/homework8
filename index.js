6893200e260ece882a133a72872b5becc6a20dbe
unknown <nmukhtar2001@gmail.com>
2023-03-03 17:54:30 -0800

    done with the home work

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ const inquirer = require('inquirer');
 const engineer = require(`./lib/engineer`)
 const intern = require (`./lib/intern`)
 const manager = require(`./lib/Manager`)
+const fs = require('fs')
+
+const generateHTML = ({ name, GitHubUsername, ID,email,schoolname}) =>
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <title>Document</title>
+</head>
+<body>
+<div class="card" style="width: 18rem;">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <h3 class="card-name">${name}</p>
+  <p class="card-text">${ID},</p>
+  <p class="card-text">${email}</p>
+  <p class="card-text">${GitHubUsername}</p>
+  <p class="card-text">${schoolname}</p>
+</div>
+</div>
+</body>
+</html>`;
+
+
+
+
+
 
 
 const menuQuestions = [
@@ -109,7 +138,6 @@ function addEmployee() {
   inquirer
     .prompt(employeeQuestions)
     .then((answers) => {
-      console.log(answers)
       if (answers.employee == `engineer`) {
         addEngineer();
       } else if (answers.employee == `intern`) {
@@ -126,6 +154,10 @@ function addEngineer() {
     .prompt(engineerQ)
     .then((answers) => {
       console.log(answers)
+      const htmlPageContent = generateHTML(answers);
+      fs.writeFile('index.html', htmlPageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created index.html!')
+      );
       console.log("inside enginerrr")
        const eng= new engineer(answers.name,answers.ID, answers.email,answers.GitHubusername)
     })
@@ -135,6 +167,10 @@ function addintern(){
   inquirer
   .prompt(internQ)
   .then((answers) => { 
+    const htmlPageContent = generateHTML(answers);
+      fs.writeFile('index.html', htmlPageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created index.html!')
+      );
     const int = new intern (answers.name,answers.ID,answers.school,answers.email)
   })
 }
@@ -143,6 +179,10 @@ function addmanagerQ(){
   inquirer
   .prompt(managerQ)
   .then((answers) => { 
+    const htmlPageContent = generateHTML(answers);
+    fs.writeFile('index.html', htmlPageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
     const man = new manager (answers.name,answers.ID,answers.officeNumber,answers.email)
   })
 }
@@ -162,5 +202,4 @@ function init() {
       }
     })
 }
-
 init()
